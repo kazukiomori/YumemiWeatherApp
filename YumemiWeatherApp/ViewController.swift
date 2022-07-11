8a4173df964e9fb451ea5afcd73aab4c4c599fb8
kazukiomori <kazuki1110170501@gmail.com>
2022-07-11 21:55:49 +0900

    APIを実装

--- YumemiWeatherApp/ViewController.swift
+++ YumemiWeatherApp/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func closeBtnTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func reloadBtnTapped(_ sender: UIButton) {
+        var weather: String = YumemiWeather.fetchWeatherCondition()
+        switch weather {
+        case "sunny" :
+            weatherImageView.image = #imageLiteral(resourceName: "iconmonstr-weather-1.pdf")
+        case "cloudy" :
+            weatherImageView.image = #imageLiteral(resourceName: "iconmonstr-weather-11.pdf")
+        case "rainy":
+            weatherImageView.image = #imageLiteral(resourceName: "iconmonstr-umbrella-1.pdf")
+        default: break
+        }
+    }
 }
 
